4383d8ab22e28af682b2aa823255ec07ccfef4c7
Noah Kremler <kremlern@gmail.com>
2024-02-25 19:33:23 -0800

    Naming conventions

--- main.py
+++ main.py
@@ -7,23 +7,28 @@ import sys
 #   https://stackoverflow.com/questions/10020885/creating-a-popup-message-box-with-an-entry-field
 
 
-class popupWindow(object):
+class PopupWindow(object):
 
     def __init__(self, master):
         top = self.top = Toplevel(master)
         self.l = Label(top, text = "Hello World")
+        self.l.pack()   # The Tkinter function pack() is simply a geometry manager
+        self.e = Entry(top)
+        self.e.pack()
+        self.b = Button(top, text = 'Ok', command = self.cleanup)
+        self.b.pack()
 
     def cleanup(self):
         self.value = self.e.get()
         self.top.destroy
 
-class mainWindow(object):
+class MainWindow(object):
 
     def __init__(self, master):
         self.master = master
 
     def popup(self):
-        self.w = popupWindow(self.master)
+        self.w = PopupWindow(self.master)
 
     def entryValue(self):
         return self.w.value
@@ -31,7 +36,7 @@ class mainWindow(object):
 
 if __name__ == "__main__":
     root = Tk()
-    m = mainWindow(root)
+    m = MainWindow(root)
     root.mainloop()
 
 
