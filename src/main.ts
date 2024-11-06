9e9a0830de3aff5895e23635df9870588669a023
Noah Kremler <kremlern@gmail.com>
2024-11-05 20:40:19 -0800

    Task entry and checkboxes implemented

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import './style.css'
+//import './style.css'
 import { v4 as uuidV4 } from "uuid" 
 
 type Task = {  // Custom type
@@ -10,7 +10,9 @@ type Task = {  // Custom type
 
 const list = document.querySelector<HTMLUListElement>("#list")
 const form = document.getElementById("new-task-form") as HTMLFormElement | null
-const input = document.querySelector<HTMLInputElement>("new-task-title")
+const input = document.querySelector<HTMLInputElement>("#new-task-title")
+
+const tasks: Task[] = []  // Array of Tasks
 
 form?.addEventListener("submit", e => {
   e.preventDefault()  // Don't want to accidentally refresh our page
@@ -18,16 +20,32 @@ form?.addEventListener("submit", e => {
   if (input?.value == "" || input?.value == null) return  // Optional Chaining (Question mark): 
                                                           // If this thing exists, give me the value. If not, return undefined
 
-  const newTask = {
+  const newTask: Task = {
     id: uuidV4(),
     title: input.value,
     completed: false,
     createdAt: new Date()
   }
 
+  tasks.push(newTask)
+
   addListItem(newTask)
+  input.value = ""  // Clear user entry after it has been entered
 })
 
 function addListItem(task: Task) {
+  const item = document.createElement("li")
+  const label = document.createElement("label")
+  const checkbox = document.createElement("input")
 
+  checkbox.addEventListener("change", () => {
+    task.completed = checkbox.checked
+    console.log(tasks)
+  })
+  checkbox.type = "checkbox"
+  checkbox.checked = task.completed
+
+  label.append(checkbox, task.title)  // Label carries the checkbox and the title of the task
+  item.append(label)  // Item carries the label
+  list?.append(item)
 }
